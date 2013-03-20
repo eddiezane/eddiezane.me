bf8ad9f1d3b0626396c0a1a7d30d82881952321f
Eddie Zaneski <eddiezane@gmail.com>
2013-03-20 09:54:13 -0400

    rails new eddiezane.me

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EddiezaneMe::Application.config.secret_token = '92067466876105ce48f39b23bee7185461a4039dd2a2e2ff8d5510e3564116667fc7bdf668516423a844c89d535707d396db4a23069bc40ab18aa14540ea032d'
