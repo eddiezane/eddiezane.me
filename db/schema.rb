9b88120a5478741a14e5cdabedb89f88cd689aaf
Eddie Zaneski <eddiezane@gmail.com>
2013-03-23 22:06:07 -0400

    Add image to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130322182514) do
+ActiveRecord::Schema.define(:version => 20130324020446) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130322182514) do
     t.string   "repo"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "image"
   end
 
 end
